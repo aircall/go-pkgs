7e979713ba68150131dcd11f440ecc497cb79ff2
Rodrigue Koffi <bonclay7@users.noreply.github.com>
2018-03-27 10:04:48 +0200

    Update EC2 Instance definition (#4)

--- aws-lambda/structs/ec2.go
+++ aws-lambda/structs/ec2.go
@@ -1,12 +1,18 @@
 package structs
 
+import "time"
+
 type EC2Instance struct {
-	InstanceId       string
-	ImageId          string
-	PrivateDnsName   string
-	KeyName          string
-	InstanceType     string
-	VpcId            string
-	PrivateIpAddress string
-	Tags             map[string]string
+	InstanceId            string
+	ImageId               string
+	PrivateDnsName        string
+	KeyName               string
+	InstanceType          string
+	VpcId                 string
+	PrivateIpAddress      string
+	IamInstanceProfile    string
+	Tags                  map[string]string
+	LaunchTime            time.Time
+	SpotInstanceRequestId string
+	State                 string
 }
